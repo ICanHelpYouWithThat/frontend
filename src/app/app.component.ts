a714370bbc17c02ebf3aa7f29d20d1b290f3ae3b
Jesse Bowden <jesse@gemr.com>
2016-11-16 22:31:52 -0500

    Add material and do i tiny ex.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import {Component, ViewEncapsulation} from '@angular/core';
+import {Observable} from 'rxjs/Rx';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  encapsulation: ViewEncapsulation.None,
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app works!';
+  title = `May I help you?... Can i help you?  Why don't I help you with that?!`;
+  cardStateFallen = false;
+  counter = 10;
+  yuem_inc = .1;
+  yuem = this.yuem_inc;
+  hoverClass = 'help_you';
+  animating = false;
+
+  onCardHover(event:any) {
+    if (!this.animating && !this.cardStateFallen) {
+      this.animating = true;
+      this.cardStateFallen = true;
+      let timer = Observable.timer(1000,1000);
+      timer.subscribe(t => (this.animating = false));
+      this.hoverClass = 'help_you runaway';
+    }
+  }
+
+  onClick(event:any) {
+    if (!this.animating && this.cardStateFallen) {
+      this.animating = true;
+      this.cardStateFallen = false;
+      let timer = Observable.timer(1000,1000);
+      timer.subscribe(t => (this.animating = false));
+      this.hoverClass = 'help_you runback';
+    }
+  }
+
+  ngOnInit() {
+    let timer = Observable.timer(1000,1000)
+      .timeInterval()
+      .pluck('interval')
+      .take(10);
+
+    timer.subscribe(t=> {
+      this.counter--;
+      this.yuem += this.yuem_inc;
+    });
+  }
 }
