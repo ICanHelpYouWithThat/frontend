85962761c4ef5a211c9f868907013015f1a4199a
Jesse Bowden <jesse@gemr.com>
2016-11-21 20:06:07 -0500

    Login screen without req.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,43 +9,7 @@ import {Observable} from 'rxjs/Rx';
 })
 
 export class AppComponent {
-  title = `May I help you?... Can i help you?  Why don't I help you with that?!`;
-  cardStateFallen = false;
-  counter = 10;
-  yuem_inc = .1;
-  yuem = this.yuem_inc;
-  hoverClass = 'help_you';
-  animating = false;
-
-  onCardHover(event:any) {
-    if (!this.animating && !this.cardStateFallen) {
-      this.animating = true;
-      this.cardStateFallen = true;
-      let timer = Observable.timer(1000,1000);
-      timer.subscribe(t => (this.animating = false));
-      this.hoverClass = 'help_you runaway';
-    }
-  }
-
-  onClick(event:any) {
-    if (!this.animating && this.cardStateFallen) {
-      this.animating = true;
-      this.cardStateFallen = false;
-      let timer = Observable.timer(1000,1000);
-      timer.subscribe(t => (this.animating = false));
-      this.hoverClass = 'help_you runback';
-    }
-  }
 
   ngOnInit() {
-    let timer = Observable.timer(1000,1000)
-      .timeInterval()
-      .pluck('interval')
-      .take(10);
-
-    timer.subscribe(t=> {
-      this.counter--;
-      this.yuem += this.yuem_inc;
-    });
   }
 }
