a839035b1260fe4e1025b8256250622af93b1a35
Zach Magaw <zmagaw@gmail.com>
2016-11-27 08:49:53 -0500

    Updated invites, tried to update dashboard component to link to invite and updated login to link

--- src/app/components/invite/invite.component.ts
+++ src/app/components/invite/invite.component.ts
@@ -1,29 +1,34 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { Router } from '@angular/router';
 import { InviteService } from '../../services/invite.service';
+import { ProfileService } from '../../services/profile.service';
 
 @Component({
   templateUrl: './invite.component.html',
   encapsulation: ViewEncapsulation.None,
   styleUrls: ['./invite.component.css'],
-  providers: [InviteService]
+  providers: [InviteService, ProfileService]
 })
 
 export class InviteComponent implements OnInit {
 
   private invitation;
   private email: string;
+  private profile;
 
-  constructor (private _inviteService: InviteService, private _router: Router) {
+  constructor (private _profileService: ProfileService, private _inviteService: InviteService, private _router: Router) {
 
     this.email = "";
 
-    this.invitation = { emails : [
-    ],
-    message: `Hey, 
-      You're getting this...
-      blah blah blah
-      Orange Menace blah `}
+    this.invitation = {
+      emails : [],
+      message: `Hey, 
+        You're getting this...
+        blah blah blah
+        Orange Menace blah `
+    }
+
+    this.profile = "";
   }
   eventHandler(event) {
     if(event.keyCode == 13){
@@ -33,15 +38,37 @@ export class InviteComponent implements OnInit {
   }
 
   addEmail() {
+    console.log("Number of invites: " + this.profile.invites)
     var anArray = this.email.split(",");
-    for(var i=0; i<anArray.length; i++)
-    {
-      this.invitation.emails.push(anArray[i]);
+    //Check the number of invites this user has to ensure they have invites left.
+    if(anArray.length <= this.profile.invites) {
+      for (var i = 0; i < anArray.length; i++) {
+        this.profile.invites--;
+        this.invitation.emails.push(anArray[i]);
+      }
+      this.email = "";
     }
-    this.email = "";
-
   }
   ngOnInit () {
+    /*
+    //will add in profile service call when getting the complete service backend stood up
+    //
+    this._profileService.getProfile()
+      .subscribe(
+        (response) => {
+          this.profile = response.profile
+        },
+        (error) => {
+          this._router.navigate(['login']);
+        }
+      )
+     */
+    // Eliminate the hardcoded Profile
+    this.profile = {
+      "name": "Zach Magaw",
+      "invites" : 10
+    }
+    // Eliminate the hardcoded Profile
 
   }
 
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -40,5 +40,7 @@ export class LoginComponent implements OnInit {
   }
 
   ngOnInit() {
+
   }
+
 }
